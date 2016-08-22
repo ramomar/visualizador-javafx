3a48df328024ad0ed26c03a63bc72cf1ccfe96f2
Omar Garza <omaarg@gmail.com>
2016-08-22 16:58:45 -0500

    Mueve VisualizadorApp a root

--- src/com/posgrado/Main.java
+++
@@ -1,8 +0,0 @@
-package com.posgrado;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/com/posgrado/VisualizadorApp.java
+++ src/com/posgrado/VisualizadorApp.java
@@ -1,5 +1,6 @@
-package com.posgrado.gui;
+package com.posgrado;
 
+import com.posgrado.gui.Visualizador;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
